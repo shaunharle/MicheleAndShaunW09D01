0659aebfc638ed752e88f1b6b3e0f12313131d99
Shaun Harle <shaunharle@Shauns-MBP.home>
2019-11-18 17:19:58 -0500

    just started changing the server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const PORT = 3003;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+	res.send("Hello World!");
 });
 
-app.listen(3000);
+app.listen(PORT);
