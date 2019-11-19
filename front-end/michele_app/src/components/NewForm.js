fb6532d6228e23166172c835de8205430c34228b
MICHELE CREANZA <michelecreanza@gmail.com>
2019-11-18 19:02:59 -0500

    up

---
+++ front-end/michele_app/src/components/NewForm.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class NewForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ""
+    };
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="name"></label>
+
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={this.handleChange}
+          value={this.state.name}
+          placeholder="add a bookmars"
+        />
+        <input type="submit" value="Add a Bookmarks" />
+      </form>
+    );
+  }
+}
+
+export default NewForm;
