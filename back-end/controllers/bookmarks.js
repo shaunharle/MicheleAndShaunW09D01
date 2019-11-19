7ef7c769e6a509084bd174cbbc0d19fd591f911d
Shaun Harle <shaunharle@Shauns-MBP.home>
2019-11-18 19:02:59 -0500

    everything on the back end looks good, still throwing a cors error. trying to merge to see if two wrongs make a right

---
+++ back-end/controllers/bookmarks.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bookmarks = express.Router();
+const Bookmark = require("../models/bookmarks.js");
+
+bookmarks.get("/", (req, res) => {
+	Bookmark.find({}, (error, foundBookmarks) => {
+		if (error) {
+			res.status(400).json({ error: error.message });
+		}
+		res.status(200).json(foundBookmarks);
+	});
+});
+
+// bookmarks.post("/", (req, res) => {
+// 	Bookmark.create(req.body, (error, createdBookmark) => {
+// 		if (error) {
+// 			res.status(400).json({ error: error.message });
+// 		}
+// 		res.status(200).send(createdBookmark);
+// 	});
+// });
+// bookmarks.put("/:id", (req, res) => {
+// 	Bookmark.findByIdAndUpdate(
+// 		req.params.id,
+// 		req.body,
+// 		{ new: true },
+// 		(error, updatedBookmark) => {
+// 			if (error) {
+// 				res.status(400).json({ error: error.message });
+// 			}
+// 			res.status(200).json(updatedBookmark);
+// 		}
+// 	);
+// });
+
+// bookmarks.delete("/:id", (req, res) => {
+// 	Bookmark.findByIdAndRemove(req.params.id, (error, deletedBookmark) => {
+// 		if (error) {
+// 			res.status(400).json({ error: error.message });
+// 		}
+// 		res.status(200).json(deletedBookmark);
+// 	});
+// });
+module.exports = bookmarks;
--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,45 @@
+// DEPENDENCIES
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+// DEPENDENCY CONFIGURATIONS
 const PORT = 3003;
 const app = express();
+const bookmarksController = require("./controllers/bookmarks.js");
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
+//MIDDLEWARE
+const whitelist = ["http://localhost:3000"];
+const corsOptions = {
+	origin: (origin, callback) => {
+		if (whitelist.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	}
+};
+// CONTROLLERS/ROUTES
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use("/bookmarks", bookmarksController);
+// DATABASE ERROR / DISCONNECTION
+mongoose.connection.on("error", error =>
+	console.log(error.message + " mongo might not be running there, chief")
+);
+mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
+
+//DATABASE CONNECTION
+mongoose.connect("mongodb://localhost:27017/bookmarks", {
+	useNewUrlParser: true
+});
+mongoose.connection.once("open", () => {
+	console.log("connected to mongoose.....");
 });
 
-app.listen(PORT);
+// app.get("/", (req, res) => {
+// 	res.send("Hello World!");
+// });
+// LISTEN
+app.listen(PORT, () => {
+	console.log("BACK END CHECKING IN HERE, ALL SYSTEMS GO");
+});
