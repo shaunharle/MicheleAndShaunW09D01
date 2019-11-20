a36dd5e8f7edc7d8ce2feff608954cf7af5c3ec3
MICHELE CREANZA <michelecreanza@gmail.com>
2019-11-20 09:21:47 -0500

    up

--- front-end/michele_app/src/App.js
+++ front-end/michele_app/src/App.js
@@ -3,6 +3,10 @@ import React, { Component } from "react";
 import "./App.css";
 import axios from "axios";
 import NewForm from "./components/NewForm.js";
+import "./css/normalize.css";
+import "./css/skeleton.css";
+import "./css/index.css";
+import Show from "./components/Show.js";
 
 let baseURL = "";
 
@@ -19,9 +23,14 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      bookmarks: []
+      bookmarks: [],
+      bookmark: {}
     };
     this.getBookmarks = this.getBookmarks.bind(this);
+    this.deleteBookmarks = this.deleteBookmarks.bind(this);
+    this.getBookmark = this.getBookmark.bind(this);
+    this.handleAddbook = this.handleAddbook.bind(this);
+    this.sayHello = this.sayHello.bind(this);
   }
   componentDidMount() {
     this.getBookmarks();
@@ -33,22 +42,69 @@ class App extends Component {
       bookmarks: data
     });
   }
+  getBookmark(bookmark) {
+    this.setState({ bookmark: bookmark });
+  }
+  handleAddbook(bookmark) {
+    const copybookmark = [...this.state.bookmarks];
+    copybookmark.unshift(bookmark);
+    this.setState({
+      bookmarks: copybookmark
+    });
+  }
+
+  async deleteBookmarks(id) {
+    await axios.delete(`${baseURL}/bookmarks/${id}`);
+    const filteredBookmarks = this.state.bookmarks.filter(bookmarks => {
+      return bookmarks._id !== id;
+    });
+
+    this.setState({
+      bookmarks: filteredBookmarks
+    });
+  }
+
+  sayHello() {
+    alert("Hello!");
+  }
 
   render() {
     return (
       <div className="container">
-        <h1>bookmars! Celebrate!</h1>
-        <NewForm getBookmarks={this.getBookmarks} baseURL={baseURL} />
         <table>
-          <tbody>
-            {this.state.bookmarks.map(bookmarks => {
-              return (
-                <tr>
-                  <td key={bookmarks._id}> {bookmarks.title}</td>
-                </tr>
-              );
-            })}
-          </tbody>
+          <header>
+            <h1>Bookmars! Great!</h1>
+          </header>
+
+          <div className="bo">
+            <h1 style={{ color: "yellow" }}>Add a new Bookmars!!</h1>
+            <NewForm handleAddbook={this.handleAddbook} baseURL={baseURL} />
+            <tbody>
+              {this.state.bookmarks.map(bookmarks => {
+                return (
+                  <tr>
+                    <td
+                      key={bookmarks._id}
+                      onMouseOver={() => this.getBookmark(bookmarks)}
+                    >
+                      <h1> CIAO {bookmarks.title}</h1>
+                      <ul>
+                        <li>{bookmarks.title}</li>
+                      </ul>
+
+                      <a href={bookmarks.url} target="_blank">
+                        {bookmarks.url}
+                      </a>
+                    </td>
+                    <td onClick={() => this.deleteBookmarks(bookmarks._id)}>
+                      X
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+            {this.state.bookmark && <Show bookmark={this.state.bookmark} />}
+          </div>
         </table>
       </div>
     );
--- front-end/michele_app/src/components/NewForm.js
+++ front-end/michele_app/src/components/NewForm.js
@@ -5,15 +5,17 @@ class NewForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: ""
+      title: "",
+      url: ""
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
+    const { name, value } = event.target;
     this.setState({
-      [event.currentTarget.title]: event.currentTarget.value
+      [name]: value
     });
   }
 
@@ -21,26 +23,37 @@ class NewForm extends Component {
     event.preventDefault();
     const baseURL = this.props.baseURL;
     const response = await axios.post(`${baseURL}/bookmarks`, {
-      title: this.state.title
+      title: this.state.title,
+      url: this.state.url
     });
     this.setState({
-      title: ""
+      title: "",
+      url: ""
     });
-    this.props.getBookmarks();
+    this.props.handleAddbook(response.data);
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label htmlFor=""></label>
+        <label htmlFor="title"></label>
 
         <input
           type="text"
-          id="name"
-          title="title"
+          id="title"
+          name="title"
           onChange={this.handleChange}
           value={this.state.title}
-          placeholder="add a bookmars"
+          placeholder="Title"
+        />
+        <label htmlFor="url"></label>
+        <input
+          type="text"
+          id="url"
+          name="url"
+          onChange={this.handleChange}
+          value={this.state.url}
+          placeholder="URL"
         />
         <input type="submit" value="Add a Bookmarks" />
       </form>
